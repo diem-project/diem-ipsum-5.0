9c585feb9493178bbd4a983cf1291860164f94be
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-15 23:39:34 +0200

    Add classes and stylesheets for the contact form

---
+++ lib/form/doctrine/dmContactPlugin/DmContactForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * DmContact form.
+ *
+ * @package    diem-ipsum
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class DmContactForm extends PluginDmContactForm
+{
+  public function configure()
+  {
+  }
+}
